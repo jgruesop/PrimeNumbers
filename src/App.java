f8137862b0835614f889c5c3750b9e4c3b787bb7
jgruesop <jhonper86@gmail.com>
2024-09-20 17:07:15 +0200

    Obtener uno o mas numeros primos desde un array

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Prime checker = new Prime();
-        checker.checkPrime(2, 1, 3, 4, 5,6,7,8,9,10,11);
+        checker.checkPrime(2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+        checker.checkPrime(20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31);
     }
 }
